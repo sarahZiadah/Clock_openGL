f6bcd8442788c6929e31726672c1147109d99f11
sarahZiadah <ziadahsarah@gmail.com>
2025-11-17 14:41:45 +0300

    last commit

--- GLProject/Main.cpp
+++ GLProject/Main.cpp
@@ -12,8 +12,8 @@
 using namespace glm;
 using namespace std;
 
-int width = 800;
-int height = 600;
+int width = 1200;
+int height = 720;
 
 vec3 cameraPos = vec3(2.0f, 1.0f, 6.0f);
 vec3 cameraFront = normalize(vec3(0.0f, 0.0f, 0.0f) - cameraPos);
@@ -54,32 +54,105 @@ int main()
     float boxW = 2.0f, boxH = 3.0f, boxDeapth = 0.5f;
     float hw = boxW / 2, hh = boxH / 2, hd = boxDeapth / 2;
 
-    Polygon boxF({ vec3(-hw, hh, hd), vec3(hw, hh, hd), vec3(hw, -hh, hd), vec3(-hw, -hh, hd) }, vec3(0.7f, 0.4f, 0.2f));
-    Polygon boxB({ vec3(-hw, hh, -hd), vec3(hw, hh, -hd), vec3(hw, -hh, -hd), vec3(-hw, -hh, -hd) }, vec3(0.6f, 0.3f, 0.15f));
-    Polygon boxL({ vec3(-hw, hh, -hd), vec3(-hw, hh, hd), vec3(-hw, -hh, hd), vec3(-hw, -hh, -hd) }, vec3(0.6f, 0.3f, 0.15f));
-    Polygon boxR({ vec3(hw, hh, -hd), vec3(hw, hh, hd), vec3(hw, -hh, hd), vec3(hw, -hh, -hd) }, vec3(0.6f, 0.3f, 0.15f));
-    Polygon boxT({ vec3(-hw, hh, -hd), vec3(hw, hh, -hd), vec3(hw, hh, hd), vec3(-hw, hh, hd) }, vec3(0.6f, 0.3f, 0.15f));
-    Polygon boxD({ vec3(-hw, -hh, -hd), vec3(hw, -hh, -hd), vec3(hw, -hh, hd), vec3(-hw, -hh, hd) }, vec3(0.6f, 0.3f, 0.15f));
+
+    Polygon boxF({ 
+        vec3(-hw, hh, hd), 
+        vec3(hw, hh, hd), 
+        vec3(hw, -hh, hd), 
+        vec3(-hw, -hh, hd) }, 
+        vec3(0.7f, 0.4f, 0.2f));
+    Polygon boxB({ 
+        vec3(-hw, hh, -hd), 
+        vec3(hw, hh, -hd), 
+        vec3(hw, -hh, -hd), 
+        vec3(-hw, -hh, -hd) }, 
+        vec3(0.6f, 0.3f, 0.15f));
+    Polygon boxL({ 
+        vec3(-hw, hh, -hd), 
+        vec3(-hw, hh, hd), 
+        vec3(-hw, -hh, hd), 
+        vec3(-hw, -hh, -hd) }, 
+        vec3(0.6f, 0.3f, 0.15f));
+    Polygon boxR({ 
+        vec3(hw, hh, -hd), 
+        vec3(hw, hh, hd), 
+        vec3(hw, -hh, hd), 
+        vec3(hw, -hh, -hd) }, 
+        vec3(0.6f, 0.3f, 0.15f));
+    Polygon boxT({ 
+        vec3(-hw, hh, -hd), 
+        vec3(hw, hh, -hd), 
+        vec3(hw, hh, hd), 
+        vec3(-hw, hh, hd) }, 
+        vec3(0.6f, 0.3f, 0.15f));
+    Polygon boxD({ 
+        vec3(-hw, -hh, -hd), 
+        vec3(hw, -hh, -hd), 
+        vec3(hw, -hh, hd), 
+        vec3(-hw, -hh, hd) }, 
+        vec3(0.6f, 0.3f, 0.15f));
 
 
-    float rodLength = 1.0f;
-    vector<vec3> rodFront = { vec3(-0.05f, rodLength / 2, 0.05f), vec3(0.05f, rodLength / 2, 0.05f), vec3(0.05f, -rodLength / 2, 0.05f), vec3(-0.05f, -rodLength / 2, 0.05f) };
-    Polygon rodF(rodFront, vec3(0.5f, 0.2f, 0.1f));
-    vector<vec3> rodBack = { vec3(-0.05f, rodLength / 2, -0.05f), vec3(0.05f, rodLength / 2, -0.05f), vec3(0.05f, -rodLength / 2, -0.05f), vec3(-0.05f, -rodLength / 2, -0.05f) };
-    Polygon rodB(rodBack, vec3(0.5f, 0.3f, 0.15f));
-    vector<vec3> rodLeft = { vec3(-0.05f, rodLength / 2, -0.05f), vec3(-0.05f, rodLength / 2, 0.05f), vec3(-0.05f, -rodLength / 2, 0.05f), vec3(-0.05f, -rodLength / 2, -0.05f) };
-    Polygon rodL(rodLeft, vec3(0.5f, 0.3f, 0.15f));
-    vector<vec3> rodRight = { vec3(0.05f, rodLength / 2, -0.05f), vec3(0.05f, rodLength / 2, 0.05f), vec3(0.05f, -rodLength / 2, 0.05f), vec3(0.05f, -rodLength / 2, -0.05f) };
-    Polygon rodR(rodRight, vec3(0.6f, 0.3f, 0.15f));
-    vector<vec3> rodTop = { vec3(-0.05f, rodLength / 2, -0.05f), vec3(0.05f, rodLength / 2, -0.05f), vec3(0.05f, rodLength / 2, 0.05f), vec3(-0.05f, rodLength / 2, 0.05f) };
-    Polygon rodT(rodTop, vec3(0.6f, 0.3f, 0.15f));
-    vector<vec3> rodBottom = { vec3(-0.05f, -rodLength / 2, -0.05f), vec3(0.05f, -rodLength / 2, -0.05f), vec3(0.05f, -rodLength / 2, 0.05f), vec3(-0.05f, -rodLength / 2, 0.05f) };
+    float rodLength = 2.0f;
+    vector<vec3> rodFront = { 
+        vec3(-0.1f, rodLength / 2, 0.1f), 
+        vec3(0.1f, rodLength / 2, 0.1f), 
+        vec3(0.1f, -rodLength / 2, 0.1f), 
+        vec3(-0.1f, -rodLength / 2, 0.1f) };
+    vector<vec3> rodBack = { 
+        vec3(-0.1f, rodLength / 2, -0.1f), 
+        vec3(0.1f, rodLength / 2, -0.1f), 
+        vec3(0.1f, -rodLength / 2, -0.1f), 
+        vec3(-0.1f, -rodLength / 2, -0.1f) };
+    vector<vec3> rodLeft = { 
+        vec3(-0.1f, rodLength / 2, -0.1f), 
+        vec3(-0.1f, rodLength / 2, 0.1f), 
+        vec3(-0.1f, -rodLength / 2, 0.1f), 
+        vec3(-0.1f, -rodLength / 2, -0.1f) };
+    vector<vec3> rodRight = { 
+        vec3(0.1f, rodLength / 2, -0.1f), 
+        vec3(0.1f, rodLength / 2, 0.1f), 
+        vec3(0.1f, -rodLength / 2, 0.1f), 
+        vec3(0.1f, -rodLength / 2, -0.1f) };
+    vector<vec3> rodTop = { 
+        vec3(-0.1f, rodLength / 2, -0.1f), 
+        vec3(0.1f, rodLength / 2, -0.1f), 
+        vec3(0.1f, rodLength / 2, 0.1f), 
+        vec3(-0.1f, rodLength / 2, 0.1f) };
+    vector<vec3> rodBottom = { 
+        vec3(-0.1f, -rodLength / 2, -0.1f), 
+        vec3(0.1f, -rodLength / 2, -0.1f), 
+        vec3(0.1f, -rodLength / 2, 0.1f), 
+        vec3(-0.1f, -rodLength / 2, 0.1f) };
+   
     Polygon rodD(rodBottom, vec3(0.6f, 0.3f, 0.15f));
+    Polygon rodF(rodFront, vec3(0.5f, 0.2f, 0.1f));
+    Polygon rodB(rodBack, vec3(0.5f, 0.3f, 0.15f));
+    Polygon rodL(rodLeft, vec3(0.5f, 0.3f, 0.15f)); 
+    Polygon rodR(rodRight, vec3(0.6f, 0.3f, 0.15f));
+    Polygon rodT(rodTop, vec3(0.6f, 0.3f, 0.15f));
 
 
     Polygon clockFace = createCircle(0.8f, 60, vec3(1.0f, 1.0f, 1.0f), hd + 0.01f);
-    mat4 clockFaceTransform = translate(mat4(1.0f), vec3(0.0f, hh - 0.9f, 0.0f));
+    mat4 clockFaceTransform = translate(mat4(1.0f), vec3(0.0f, hh - 1.2f, 0.0f));
     clockFace.transformation(clockFaceTransform);
+
+    float handLength = 0.7f;   
+    float handWidth = 0.03f;  
+
+    vector<vec3> handFront = {
+    vec3(-handWidth, 0.0f, 0.3f),   
+    vec3(handWidth,  0.0f, 0.3f),
+    vec3(handWidth,  handLength, 0.3f),
+    vec3(-handWidth, handLength, 0.3f)
+    };
+
+
+    Polygon clockHand(handFront, vec3(0.2f, 0.2f, 0.2f));
+
+    mat4 handTransform = translate(mat4(1.0f), vec3(0.0f, hh - 1.2f, 0.0f));
+    clockHand.transformation(handTransform);
+
 
     mat4 projection = perspective(radians(45.0f), (float)width / height, 0.1f, 100.0f);
     ourShader.setMat4("projection", projection);
@@ -88,17 +161,32 @@ int main()
     {
         processInput(window);
 
-        glClearColor(0, 0, 0, 1);
+        glClearColor(0.2, 0.2, 0.2, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         mat4 view = lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         ourShader.setMat4("view", view);
 
+        mat4 sceneTransform = mat4(1.0f);
+        sceneTransform = translate(sceneTransform, vec3(0.0f, 0.0f, -2.0f));
+
+        boxF.transformation(sceneTransform);
+        boxB.transformation(sceneTransform);
+        boxL.transformation(sceneTransform);
+        boxR.transformation(sceneTransform);
+        boxT.transformation(sceneTransform);
+        boxD.transformation(sceneTransform);
+
+        clockFace.transformation(sceneTransform);
+        clockHand.transformation(sceneTransform);
+
+
         float angle = sin(glfwGetTime() * 2.0f) * radians(25.0f);
         mat4 pendulumTransform = mat4(1.0f);
-        pendulumTransform = translate(pendulumTransform, vec3(0.0f, -hh, 0.0f)); // عند أسفل الصندوق
+        pendulumTransform = translate(pendulumTransform, vec3(0.0f, -hh, 0.0f)); 
         pendulumTransform = rotate(pendulumTransform, angle, vec3(0, 0, 1));
         pendulumTransform = translate(pendulumTransform, vec3(0.0f, -rodLength / 2, 0.0f));
+        pendulumTransform = sceneTransform * pendulumTransform;
 
         rodF.transformation(pendulumTransform);
         rodB.transformation(pendulumTransform);
@@ -125,6 +213,9 @@ int main()
         rodR.draw(ourShader);
         rodT.draw(ourShader);
         rodD.draw(ourShader);
+
+        clockHand.draw(ourShader);
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
